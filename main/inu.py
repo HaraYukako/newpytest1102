fde16f89da13f6bf438c29cfb12448e8b10bc1c2
原 夕香子 <HARAY1@tss.co.jp>
2022-11-04 10:49:36 +0900

    1104

---
+++ main/inu.py
@@ -0,0 +1,68 @@
+
+ #足し算
+
+def add(x, y):
+
+        return x + y
+
+    
+
+#引き算
+
+def subtract(x, y):
+
+        return x - y
+
+    
+
+#掛け算
+
+def multiply(x, y):
+
+        return x * y
+
+    
+
+#割り算
+
+def divide(x, y):
+
+        return x / y
+
+    
+
+    # #メイン処理
+
+    # if __name__ == '__main__':
+
+    
+
+    #     #変数を設定
+
+    #     number_one = 32
+
+    #     number_two = 16
+
+    
+
+    #     #足し算
+
+    #     print(add(number_one, number_two))
+
+    
+
+    #     #引き算
+
+    #     print(subtract(number_one, number_two))
+
+    
+
+    #     #掛け算
+
+    #     print(multiply(number_one, number_two))
+
+        
+
+    #     #割り算
+
+    #     print(divide(number_one, number_two))
---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+
+from main.inu import add,subtract,multiply,divide
+
+def test_add():
+    assert add(32, 16) == 48
+
+def test_subtract():
+    assert subtract(50, 13) == 37
+
+def test_multiply():
+    assert multiply(5, 2) == 10
+
+def test_divide():
+    assert divide(4, 2) == 2
