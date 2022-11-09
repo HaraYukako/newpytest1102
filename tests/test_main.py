f8e27fc0c5b4d18e14a6a76de62753d737b67dc2
原 夕香子 <HARAY1@tss.co.jp>
2022-11-09 15:53:01 +0900

    1109

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@
 from main.inu import add,subtract,multiply,divide
 
 def test_add():
-    assert add(32, 16) == 48
+    assert add(2, 5) == 7
 
 def test_subtract():
     assert subtract(50, 13) == 37
